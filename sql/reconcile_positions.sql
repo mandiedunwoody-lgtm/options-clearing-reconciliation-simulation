89b6b8d37425e7b7e331f43bcf001317dda34933
Mandie Dunwoody <mandiedunwoody@gmail.com>
2026-09-22 11:05:10 -0400

    Add SQL query to reconcile expected and actual positions

--- sql/reconcile_positions.sql
+++ sql/reconcile_positions.sql
@@ -1,1 +1,11 @@
+SELECT 
+    e.option_id,
+    e.account_id,
+    e.quantity AS expected_qty,
+    a.quantity AS actual_qty,
+    (a.quantity - e.quantity) AS break_amount
+FROM positions_expected e
+LEFT JOIN positions_actual a
+    ON e.option_id = a.option_id
+WHERE (a.quantity - e.quantity) <> 0;
 
